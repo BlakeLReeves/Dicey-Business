0f1ae08f07ff6ecb1d7ae70f96b3281a4fa7132f
blakereeves19 <blake.reeves19@gmail.com>
2019-01-14 14:20:09 -0600

    Added buttons and psuedo class

---
+++ app.js
@@ -0,0 +1,57 @@
+let dieArray = [];
+
+class Die {
+    constructor() {
+        this.value;
+        this.roll();
+        this.div = $(`<div></div>`);
+        this.div.text(this.value);
+        $(`#dieDiv`).append(this.div);
+        this.div.css({
+            "background-color": "grey",
+            "height": "100px",
+            "width": "100px",
+            "float": "left",
+            "margin-top": "1em",
+            "margin-left": "1em",
+            "border-radius": "15px",
+            "text-align": "center",
+            "line-height": "100px"
+        });
+
+        this.div.click(() => {
+            this.roll();
+            this.div.text(this.value);
+        })
+
+        $(`#rollBtn`).click(() => {
+            dieArray.forEach(() => {
+                this.roll();
+                this.div.text(this.value);
+            })
+        })
+    }
+
+    roll() {
+        this.value = Math.floor((Math.random() * 6) + 1);
+    }
+}
+
+$(`#generateBtn`).click(() => {
+    let die = new Die();
+    console.log(die);
+    dieArray.push(die);
+});
+
+$(`#sumDice`).click(() => {
+    let result = sumDice(dieArray);
+    alert(result);
+})
+
+let sumDice = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i].value;
+    }
+    return sum;
+}
